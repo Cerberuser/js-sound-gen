4da1f7d5d7bdfecf3c7fa56f35ed491e7122095d
k.bagrov <k.bagrov@g.nsu.ru>
2018-12-14 01:00:02 +0700

    Added WAV-file download (code copied from https://gist.github.com/asanoboy/3979747 and refactored for TS)

--- www/main.ts
+++ www/main.ts
@@ -1,5 +1,7 @@
 // import Worker from 'worker-loader!./gen-worker';
 
+import {make_wav} from "./makewav";
+
 const worker = new Worker('./gen-worker.js');
 worker.onerror = (ev: ErrorEvent) => {
     debugger;
@@ -14,13 +16,16 @@ worker.onmessage = (ev: MessageEvent) => {
         const sampleRate = context.sampleRate;
         const buffer = context.createBuffer(1, sampleRate * 6, sampleRate);
         const source = context.createBufferSource();
+        const arr = new Float32Array(ev.data.buf as ArrayBuffer);
 
-        buffer.copyToChannel(new Float32Array(ev.data.buf as ArrayBuffer), 0, 0);
+        buffer.copyToChannel(arr, 0, 0);
         console.log(buffer.getChannelData(0));
         source.buffer = buffer;
         source.connect(context.destination);
 
         source.start();
+
+        make_wav(sampleRate, arr);
     } else {
         initialized = true;
         worker.postMessage({sampleRate: context.sampleRate});
